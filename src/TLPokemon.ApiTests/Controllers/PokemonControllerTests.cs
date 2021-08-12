98d320d5a84f59882b6615de0bede8b1e6098a31
Sham <shamx9ir@gmail.com>
2021-08-12 18:09:23 +0100

    add tests

---
+++ src/TLPokemon.ApiTests/Controllers/PokemonControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TLPokemon.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Autofac;
+using TLPokemon.Api.Services.Pokemon;
+using Moq;
+using TLPokemon.Api.Models;
+using TLPokemon.ApiTests.Helpers;
+using TLPokemon.Api.Services.Network;
+
+namespace TLPokemon.Api.Controllers.Tests
+{
+    [TestClass()]
+    public class PokemonControllerTests
+    {
+        [TestMethod()]
+        public void Get_Calls_PokemonServiceGet_WithPokemonType()
+        {
+            using (var mock = AutoMock.GetLoose(builder =>
+            {
+                builder.RegisterType<PokemonController>();
+
+            }))
+            {
+                var lifetimeScopeMock = new Mock<ILifetimeScope>();
+                var networkServiceMock = new Mock<INetworkService>();
+
+                var pokemonServiceMock = new Mock<PokemonService>(lifetimeScopeMock.Object, networkServiceMock.Object);
+                pokemonServiceMock.Setup(m => m.Get<Pokemon>(It.IsAny<string>())).Returns(new Pokemon("mewtwo", "description", "habitat", true));
+
+                var pokemonService = pokemonServiceMock.Object;
+                // var controller = mock.Create<PokemonController>((IPokemonService)pokemonService);
+                var controller = new PokemonController(pokemonService);
+                var result = controller.Get("mewtwo");
+
+                pokemonServiceMock.Verify(m => m.Get<Pokemon>(It.IsAny<string>()), Times.Once);
+
+            }
+        }
+
+
+    }
+}
---
+++ src/TLPokemon.ApiTests/Helpers/MockHelper.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Extras.Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLPokemon.ApiTests.Helpers
+{
+    public static class MockHelper
+    {
+        public static TypedParameter GetTypedParameter(this object obj)
+        {
+            return new TypedParameter(obj.GetType(), obj);
+        }
+
+        public static T Create<T>(this AutoMock mock, params object[] parameters)
+        {
+            var paramList = new List<Parameter>();
+            foreach(var p in parameters)
+            {
+                paramList.Add(p.GetTypedParameter());
+            }
+            return mock.Create<T>(paramList.ToArray());
+        }
+    }
+}
