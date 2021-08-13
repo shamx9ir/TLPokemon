987d14252ecf44c509ae4a60ae9d8e4b92368d7d
Sham <shamx9ir@gmail.com>
2021-08-13 20:33:26 +0100

    Add translation service tests

--- src/TLPokemon.ApiTests/Services/Translation/TranslationServiceBaseTests.cs
+++ src/TLPokemon.ApiTests/Services/Translation/TranslationServiceBaseTests.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TLPokemon.Api.Services.Translation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TLPokemon.Api.Services.Network;
+using Moq;
+using Autofac.Extras.Moq;
+using Autofac;
+using TLPokemon.ApiTests.Helpers;
+using System.Net.Http;
 
 namespace TLPokemon.Api.Services.Translation.Tests
 {
@@ -12,9 +12,80 @@ namespace TLPokemon.Api.Services.Translation.Tests
     public class TranslationServiceBaseTests
     {
         [TestMethod()]
-        public void TranslateTest()
+        public void Translate_ReturnsTranslatedText()
         {
-            Assert.Fail();
+            var expectedString = "Thee did giveth mr. Tim a hearty meal,  but unfortunately what he did doth englut did maketh him kicketh the bucket.";
+            var pokemonApiJson = @"{
+                  ""success"": {
+                    ""total"": 1
+                  },
+                  ""contents"": {
+                    ""translated"": """ + expectedString + @""",
+                    ""text"": ""You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die."",
+                    ""translation"": ""shakespeare""
+                  }
+                }";
+
+            var networkServiceMock = new Mock<INetworkService>();
+            networkServiceMock.Setup(m => m.PostJsonString(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(pokemonApiJson);
+
+            using (var mock = AutoMock.GetLoose(builder =>
+            {
+                builder.RegisterInstance(networkServiceMock.Object).As<INetworkService>();
+            }))
+            {
+                var inputText = "Input text";
+                var translationService = mock.Create<TranslationServiceBase>("https://api.funtranslations.com/translate/shakespeare.json");
+                var result = translationService.Translate(inputText);
+
+                Assert.AreEqual(expectedString, result);
+            }
+        }
+
+        [TestMethod()]
+        public void Translate_NoTranslation()
+        {
+            var pokemonApiJson = @"{
+                  ""success"": {
+                    ""total"": 0
+                  },
+                  ""contents"": {
+                  }
+                }";
+
+            var networkServiceMock = new Mock<INetworkService>();
+            networkServiceMock.Setup(m => m.PostJsonString(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(pokemonApiJson);
+
+            using (var mock = AutoMock.GetLoose(builder =>
+            {
+                builder.RegisterInstance(networkServiceMock.Object).As<INetworkService>();
+            }))
+            {
+                var inputText = "Input text";
+                var translationService = mock.Create<TranslationServiceBase>("https://api.funtranslations.com/translate/shakespeare.json");
+                var result = translationService.Translate(inputText);
+
+                Assert.AreEqual(inputText, result);
+            }
+        }
+
+        [TestMethod()]
+        public void Translate_Exception()
+        {
+            var networkServiceMock = new Mock<INetworkService>();
+            networkServiceMock.Setup(m => m.PostJsonString(It.IsAny<string>(), It.IsAny<HttpContent>())).Throws(new System.Exception());
+
+            using (var mock = AutoMock.GetLoose(builder =>
+            {
+                builder.RegisterInstance(networkServiceMock.Object).As<INetworkService>();
+            }))
+            {
+                var inputText = "Input text";
+                var translationService = mock.Create<TranslationServiceBase>("https://api.funtranslations.com/translate/shakespeare.json");
+                var result = translationService.Translate(inputText);
+
+                Assert.AreEqual(inputText, result);
+            }
         }
     }
 }
